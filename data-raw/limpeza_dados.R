4b296967987239c043501b8537ef423bef06e456
Monoxido45 <lucruz45.cab@gmail.com>
2022-03-20 14:28:05 -0300

    limpeza de dados finalizada

--- data-raw/limpeza_dados.R
+++ data-raw/limpeza_dados.R
@@ -19,11 +19,27 @@ drive_download("Censo_SUAS_2019_CREAS_Dados_Gerais_divulgacao.csv",
 dados_nao_limpos_geral <- "data-raw/Censo_SUAS_2019_CREAS_Dados_Gerais_divulgacao.csv" |>
   read.csv(sep = ";")
 
+# funcoes auxiliares para categorizacoes e unificacoes
+# para a variavel de violencia
+muda_q <- function(qs){
+  bools <- c(qs[1] == "Sim", 
+             qs[2] == "Sim", 
+             qs[3] == "Sim", 
+             qs[4] == "Sim")
+  return(as.character(sum(bools)))
+}
+# para as salas
+muda_salas <- function(q6){
+  case_when(q6 == 0 ~ "0",
+            q6 == 1 ~ "1",
+            q6 >= 2 ~ "2")
+}
+
+
 # primeiro selecionando apenas as variaveis de interesse
+# mudando alguma variaveis pegando variaveis de violencia fisica
 dados_nao_limpos_geral <- dados_nao_limpos_geral |>
   select(c(IBGE:Q_completo), 
-         q0_9, 
-         q0_10,
          q0_15,
          q1,
          Latitude,
@@ -37,52 +53,101 @@ dados_nao_limpos_geral <- dados_nao_limpos_geral |>
          c(q9_1:q9_12),
          q10_1,
          q10_2,
-         c(q11_1:q11_22),
-         q12_1_0,
-         q12_2_0,
-         q12_3_0,
-         q12_4_0,
-         q12_5_0,
-         q12_6_0,
-         q12_7_0,
-         q12_8_0,
-         q12_9_0,
-         q12_10_0,
-         q12_11_0,
-         q12_12_0,
-         q12_13_0,
-         q12_14_0,
-         q12_15_0,
-         q12_16_0,
-         q12_17_0,
-         q13_1_0,
-         q13_2_0,
-         q13_3_0,
-         q14_1,
-         q14_2,
-         q15,
-         q27,
-         q30,
-         q31,
-         q33,
+         c(q12_1_1:q12_17_0),
          q42,
-         q44_0,
-         q48_0,
-         q49_0,
-         q50_0,
-         q55_0,
-         q57)
-    
+         q57) |>
+  rowwise() |>
+  mutate(
+    data_creas = q0_15,
+    tipo_creas = q1,
+    funcionamento_dias = q2_1,
+    functionamento_horas = q2_2,
+    imovel_comp = q3,
+    salas_5_pessoas = muda_salas(q6_1),
+    salas_6_14_pessoas = muda_salas(q6_2),
+    salas_15_29_pessoas = muda_salas(q6_3),
+    salas_30_pessoas = muda_salas(q6_4),
+    salas_coord = muda_salas(q6_5),
+    banheiros = case_when(q6_6 == "Não" ~ "0",
+                          q6_6 == "Sim" ~ "1",
+                          q6_6 == "2" ~ "2",
+                          TRUE ~ "3"),
+    # renomeando
+    recepcao = q6_7,
+    cozinha = q6_8,
+    almoxarifado = q6_9,
+    espaco_externo = q6_10,
+    # acesso a rampas e estabelecimentos
+    rampas_calcada = q7_1,
+    rotas_espacos = q7_2,
+    rota_banheiro = q7_3,
+    banheiro_adaptado = q7_4,
+    outras_adaptacoes = q8_0,
+    # condensando as questoes dos materiais
+    telefone = case_when(q9_1 == "Sim" | q9_2 == "Sim" ~ "Sim",
+                         TRUE ~ "Não"),
+    # impressora
+    impressora = q9_3,
+    # tv, equip de som e dvd
+    tv_equip = case_when(q9_4 == "Sim" | q9_5 == "Sim" | q9_6 == "Sim" ~ "Sim",
+                         TRUE ~ "Não"),
+    # datashow
+    datashow = q9_7,
+    # veiculo
+    veiculo = case_when(q9_8 == "Sim" | q9_9 == "Sim" ~ "Sim",
+                        TRUE ~ "Não"),
+    # materiais de recreacao e consulta
+    materiais_recreacao = case_when(
+      q9_10 == "Sim" | q9_11 == "Sim" | q9_12 == "Sim" ~ "Sim",
+      TRUE ~ "Não"),
+    # 0 = sem computador, 1 = com computador, mas sem internet
+    # 2 = 1 a 5 comp com internet
+    comp_qtd = case_when(q10_1 == 0 | q10_2 == 0 ~ "1",
+                         q10_2 > 0 & q10_2 <= 5 ~ "2",
+                         q10_2 > 5 & q10_2 <= 10 ~ "3",
+                         q10_2 > 10 & q10_2 <= 15 ~ "4",
+                         q10_2 > 15 ~ "5"),
+    # numero indicando quantas faixas atende
+    violencia_fisica = muda_q(c_across(q12_1_1:q12_1_4)),
+    violencia_psico = muda_q(c_across(q12_2_1:q12_2_4)),
+    violencia_sexual = muda_q(c_across(q12_3_1:q12_3_4)),
+    exploracao_sexual = muda_q(c_across(q12_4_1:q12_4_4)),
+    abandono = muda_q(c_across(q12_5_1:q12_5_4)),
+    violencia_patri = muda_q(c_across(q12_6_1:q12_6_4)),
+    trafico_pessoas = muda_q(c_across(q12_7_1:q12_7_4)),
+    trab_infantil = muda_q(c_across(q12_8_1:q12_8_4)),
+    sit_rua = muda_q(c_across(q12_9_1:q12_9_4)),
+    disc_sexual = muda_q(c_across(q12_10_1:q12_10_4)),
+    disc_racial = muda_q(c_across(q12_11_1:q12_11_4)),
+    violencia_def = muda_q(c_across(q12_12_1:q12_12_4)),
+    acolhimento = muda_q(c_across(q12_13_1:q12_13_4)),
+    priv_lib = muda_q(c_across(q12_16_1:q12_16_4)),
+    cadastro = q42,
+    coord_creas = q57,
+    .keep = "unused"
+  ) |>
+  select(-c(q12_1_0:q12_17_0))
 
+# cruzando infos para obter o numero de funcionarios em diferentes areas
+dados_rh <- "data-raw/Censo_SUAS_2019_RH_CREAS_divulgação.xlsx" |>
+  readxl::read_xlsx()
 
-# limpando, juntando variaveis, mudando tudo:
-# primeiramente tirando algumas variaveis nao uteis
-dados_nao_limpos_rh <- dados_nao_limpos_rh |>
-  select(-c(d_58_8bin1, d_58_8bin2, d_58_8bin3,
-            d_58_9bin1, d_58_9bin2, d_58_9bin3, d_58_9bin4, d_58_9bin5,
-            d_58_10bin1, d_58_10bin2, d_58_10bin3, d_58_10bin4)) |>
-  select(- c(d_58_14_1bin_Gestão:d_58_16_3bin_Outros))
-
-
-
-
+dados_nao_limpos_geral <- dados_nao_limpos_geral |>
+  left_join(dados_rh |> select(NU_IDENTIFICADOR, d_58_8bin1_sum:d_58_8bin3_sum) |>
+              distinct(NU_IDENTIFICADOR, .keep_all = TRUE),
+            by = "NU_IDENTIFICADOR") |>
+  mutate(func_fund = case_when(is.na(d_58_8bin1_sum) ~ "Sem info",
+                               d_58_8bin1_sum == 0 ~ "0",
+                               d_58_8bin1_sum == 1 ~ "1",
+                               d_58_8bin1_sum > 1 ~ "2"),
+         func_med = case_when(is.na(d_58_8bin1_sum) ~ "Sem info",
+                              d_58_8bin2_sum == 0 ~ "0",
+                              d_58_8bin2_sum == 1 ~ "1",
+                              d_58_8bin2_sum == 2 ~ "2",
+                              d_58_8bin2_sum > 2 ~ "3"),
+         func_alto = case_when(is.na(d_58_8bin1_sum) ~ "Sem info",
+                               d_58_8bin3_sum < 3 ~ "1",
+                               d_58_8bin3_sum >= 3 & d_58_8bin3_sum < 6, "2",
+                               TRUE, "3"))
+# salvando esses dados
+dados_nao_limpos_geral |>  write_rds("analise/dados_gerais_CREAS.rds")
